05ea2e6e6bad137eb4847af5eae0f54ef8e13681
kiwikumbhar029 <108450409+kiwikumbhar029@users.noreply.github.com>
2022-07-07 15:25:23 +0530

    Add files via upload

---
+++ IdentifyDataTypes.java
@@ -0,0 +1,40 @@
+class IdentifyDataTypes
+	
+{
+		public static void main(String[] args)
+		{
+
+
+            String name = "Paul";
+            System.out.println(name);
+            
+            String brand = "idol";
+            System.out.println(brand);
+
+            double processorSpeedInGHz = 2.66;
+            System.out.println(processorSpeedInGHz);
+
+            double ramSizeInGB = 8;
+            System.out.println(ramSizeInGB);
+
+            int harddiskSizeInGB = 500;
+            System.out.println(harddiskSizeInGB);
+        
+            int monitorInInch = 15;
+            System.out.println(monitorInInch);
+            
+            double price = 1760.55;
+            System.out.println(price);
+
+            char servicePlan = 'C';
+            System.out.println(servicePlan);
+
+            boolean onSiteService = true;
+            System.out.println(onSiteService );
+
+            boolean extendedWarranty = false;
+            System.out.println(extendedWarranty);
+            
+            }
+
+}
